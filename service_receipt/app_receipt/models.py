80ac6c3a293d87ae3feedb2269c05c7f33ecc57f
ElenaGlu <shishalovae@mail.ru>
2024-06-17 20:53:55 +0500

    add models

--- service_receipt/app_receipt/models.py
+++ service_receipt/app_receipt/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Printer(models.Model):
+    title = models.CharField(max_length=100)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    title = models.CharField(max_length=100)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+
+STATUS = {
+    "CREATE": "in the creation queue",
+    "READY": "ready to print",
+    "RELEASE": "release"
+}
+
+
+class Receipt(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=7, choices=STATUS)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)

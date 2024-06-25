3791b827bb8b7b93664c257cc2d2961cdc5d8f14
ElenaGlu <shishalovae@mail.ru>
2024-06-25 19:58:17 +0500

    fix the task logic

--- service_receipt/app_receipt/tasks.py
+++ service_receipt/app_receipt/tasks.py
@@ -1,14 +1,17 @@
+from typing import Dict
+
 from celery import shared_task
 from django.template.loader import get_template
 import pdfkit
 
-from django.http import HttpResponse
+from app_receipt.models import Order
 
 
 @shared_task()
-def generate_PDF_task(data):
+def generate_PDF_task(request: Dict[str, str]) -> Dict[str, bool]:
     """
     Generate a PDF file from an HTML template
+    :param request: dict containing keys - title, restaurant
     :return:
     """
     options = {
@@ -22,14 +25,13 @@ def generate_PDF_task(data):
         'custom-header': [('Accept-Encoding', 'gzip')],
         'no-outline': None
     }
-
+    title = request['title']
     template = get_template("pdf_template.html")
-    template.render(context=data)
-    pdfkit.from_file('/home/elena/lena/receipt_generation/service_receipt/app_receipt/templates/pdf_template.html', '/home/elena/lena/receipt_generation/service_receipt/app_receipt/templates/out.pdf', options=options)
-
-    pdf = open("/home/elena/lena/receipt_generation/service_receipt/app_receipt/templates/out.pdf", encoding="utf8", errors='ignore')
-
-    response = HttpResponse(pdf.read(), content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=output.pdf'
-    # pdf.close()
-    return response
+    html = template.render(request)
+    pdfkit.from_string(
+        html,
+        f'/home/elena/lena/receipt_generation/service_receipt/app_receipt/media/PDF/{title}.pdf',
+        options=options
+        )
+    Order.objects.filter(title=title).update(status='READY')
+    return {"status": True}

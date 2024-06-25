204f7eb7ea6ab2449c4eccd55fd0c457637dcdf5
ElenaGlu <shishalovae@mail.ru>
2024-06-25 19:55:55 +0500

    add an exception handler

---
+++ service_receipt/exceptions.py
@@ -0,0 +1,22 @@
+class AppExceptions(Exception):
+    pass
+
+
+class AppError(AppExceptions):
+    pass
+
+
+class ErrorType:
+    PRINTER_ERROR = {
+        'status_code': 409,
+        'summary': 'Conflict',
+    }
+
+    ORDER_ERROR = {
+        'status_code': 400,
+        'summary': 'Bad Request',
+    }
+    RECEIPT_ERROR = {
+        'status_code': 404,
+        'summary': 'Not found',
+    }

962b38a2c5d0aaa992e8066258f8ce1a84ea2291
ElenaGlu <shishalovae@mail.ru>
2024-06-26 19:23:46 +0500

    set up order filtering by printer and status

--- service_receipt/app_receipt/admin.py
+++ service_receipt/app_receipt/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_receipt.models import Restaurant, Printer, Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'printer')
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    pass
+
+
